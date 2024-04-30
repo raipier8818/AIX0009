4e5df1f49e3da6ebf9f2f7ba8ee1b8144497a195
Lake <lake@Lakeui-Macmini.local>
2024-04-30 21:22:34 +0900

    create orderbook.py

--- project1/orderbook.py
+++ project1/orderbook.py
@@ -53,8 +53,8 @@ def get_orderbook(market: str, level: int) -> pd.DataFrame:
         return None
         
     # get timestamp, bids, asks from response
-    timestamp = response.json()["data"]["timestamp"]
-    timestamp = str(datetime.fromtimestamp(int(timestamp[:-3]))) + '.' + timestamp[-3:]
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    
     bids = response.json()["data"]["bids"]
     asks = response.json()["data"]["asks"]
 
